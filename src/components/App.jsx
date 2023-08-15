77efe6721ca414c249c7196bb28c4171b4f01549
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-08-15 10:58:45 +0300

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Section } from './Section/Section';
+import { Form } from './Form/Form';
+import { Contacts } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook">
+        <Form />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <Contacts />
+      </Section>
+    </>
   );
 };
+
