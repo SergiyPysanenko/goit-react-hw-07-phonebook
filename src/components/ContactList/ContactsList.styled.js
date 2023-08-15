cfbb7884f8aea0c2fd01244f85ba31fe0c912590
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-08-15 17:59:38 +0300

    Fix

--- src/components/ContactList/ContactsList.styled.js
+++ src/components/ContactList/ContactsList.styled.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const List = styled.ul`
   padding-left: 30px;
@@ -38,23 +38,5 @@ export const Button = styled.button`
   }
 `;
 
-const spin = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
 
-export const Spinner = styled.div`
-  border: 4px solid rgba(0, 0, 0, 0.1);
-  border-top-color: #333;
-  border-radius: 50%;
-  width: 24px;
-  height: 24px;
-  margin-bottom: 20px;
-  margin-left: 70px;
 
-  animation: ${spin} 1s ease-in-out infinite;
-`;
